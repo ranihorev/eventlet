9c1d582f1170547de6f5ad01bf70e266ec28c161
Sergey Shepelev <temotor@gmail.com>
2016-11-29 08:48:33 +0300

    WIP https://github.com/eventlet/eventlet/issues/358

--- tests/hub_test.py
+++ tests/hub_test.py
@@ -20,7 +20,7 @@ def noop():
 class TestTimerCleanup(tests.LimitedTestCase):
     TEST_TIMEOUT = 2
 
-    @skip_with_pyevent
+    @tests.skip_with_pyevent
     def test_cancel_immediate(self):
         hub = hubs.get_hub()
         stimers = hub.get_timers_count()
@@ -34,7 +34,7 @@ class TestTimerCleanup(tests.LimitedTestCase):
         self.assert_less_than_equal(hub.get_timers_count(), 1000 + stimers)
         self.assert_less_than_equal(hub.timers_canceled, 1000)
 
-    @skip_with_pyevent
+    @tests.skip_with_pyevent
     def test_cancel_accumulated(self):
         hub = hubs.get_hub()
         stimers = hub.get_timers_count()
@@ -51,7 +51,7 @@ class TestTimerCleanup(tests.LimitedTestCase):
         self.assert_less_than_equal(hub.get_timers_count(), 1000 + stimers)
         self.assert_less_than_equal(hub.timers_canceled, 1000)
 
-    @skip_with_pyevent
+    @tests.skip_with_pyevent
     def test_cancel_proportion(self):
         # if fewer than half the pending timers are canceled, it should
         # not clean them out
@@ -149,7 +149,7 @@ class TestExceptionInMainloop(tests.LimitedTestCase):
 
 class TestExceptionInGreenthread(tests.LimitedTestCase):
 
-    @skip_unless(greenlets.preserves_excinfo)
+    @tests.skip_unless(greenlets.preserves_excinfo)
     def test_exceptionpreservation(self):
         # events for controlling execution order
         gt1event = eventlet.Event()
@@ -207,7 +207,7 @@ class TestExceptionInGreenthread(tests.LimitedTestCase):
 class TestHubBlockingDetector(tests.LimitedTestCase):
     TEST_TIMEOUT = 10
 
-    @skip_with_pyevent
+    @tests.skip_with_pyevent
     def test_block_detect(self):
         def look_im_blocking():
             import time
@@ -218,8 +218,8 @@ class TestHubBlockingDetector(tests.LimitedTestCase):
         self.assertRaises(RuntimeError, gt.wait)
         debug.hub_blocking_detection(False)
 
-    @skip_with_pyevent
-    @skip_if_no_itimer
+    @tests.skip_with_pyevent
+    @tests.skip_if_no_itimer
     def test_block_detect_with_itimer(self):
         def look_im_blocking():
             import time
@@ -357,3 +357,7 @@ def test_kqueue_unsupported():
     # https://github.com/eventlet/eventlet/issues/38
     # get_hub on windows broken by kqueue
     tests.run_isolated('hub_kqueue_unsupported.py')
+
+    
+def test_hub_silent_exception():
+    tests.run_isolated('hub_silent_exception.py')
